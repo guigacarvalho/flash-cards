671cb3f3994687990d712e885b3735e56a4bbf46
Guilherme B. Carvalho <guilherme.carvalho@gmail.com>
2017-10-25 07:47:56 -0700

    Creating app shell and stubs for the components

--- App.js
+++ App.js
@@ -1,18 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Platform } from 'react-native';
+import { TabNavigator } from 'react-navigation'
+import { FontAwesome } from '@expo/vector-icons'
+import initialState from './src/utils/asyncStorage'
+
+// TODO: Extract in utils/colors
+const white = '#fff'
+const black = '#000'
+const shadow = 'rgba(0, 0, 0, 0.24)'
+
+// TODO: Fill in functionality and extract in its own file
+const DeckList = () => (
+  <View style={styles.container}>
+      <Text>DecksList!</Text>
+      <Text>{JSON.stringify(initialState)}</Text>
+  </View>
+);
+
+// TODO: Fill in functionality and extract in its own file
+const Quizes = () => (
+  <View style={styles.container}>
+    <Text>Quizes!</Text>
+  </View>
+);
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({tintColor}) => <FontAwesome name='clone' size={28} color={tintColor}/>
+    }
+  },
+  Quizes: {
+    screen: Quizes,
+    navigationOptions: {
+      tabBarLabel: 'Quizes',
+      tabBarIcon: ({tintColor}) => <FontAwesome name='question-circle-o' size={28} color={tintColor}/>
+    }
+  }, 
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? black : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : black,
+      shadowColor: shadow,
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Tabs />
     );
   }
 }
 
+// TODO: Convert to styled components
 const styles = StyleSheet.create({
   container: {
     flex: 1,
