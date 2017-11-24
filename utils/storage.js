549ee54e8c641463d0490f16f2915ee323ea58f1
Guilherme B. Carvalho <guilherme.carvalho@gmail.com>
2017-11-23 16:08:08 -0800

    Adding cards and decks

--- utils/storage.js
+++ utils/storage.js
@@ -1,67 +1,70 @@
 
 import { AsyncStorage } from 'react-native'
 
-export async function
-   getDecks() {
+export async function getDecks() {
     let response = await AsyncStorage.getItem('listOfDecks');
-    let lodecks = await JSON.parse(response) || {};
-    return lodecks;
+    let decks = await JSON.parse(response) || {};
+    return decks;
   }
 
-  export async function addInitialDecks () { 
-    let listOfDecks =  {
-      React: {
-        title: 'React',
-        questions: [
-          {
-            question: 'What is React?',
-            answer: 'A library for managing user interfaces'
-          },
-          {
-            question: 'Where do you make Ajax requests in React?',
-            answer: 'The componentDidMount lifecycle event'
-          }
-        ]
-      },
-      JavaScript: {
-        title: 'JavaScript',
-        questions: [
-          {
-            question: 'What is a closure?',
-            answer: 'The combination of a function and the lexical environment within which that function was declared.'
-          }
-        ]
-      }
-    };
+export async function addInitialDecks () { 
+  let listOfDecks =  {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.'
+        }
+      ]
+    }
+  };
 
-      await AsyncStorage.setItem('listOfDecks', 
-        JSON.stringify(listOfDecks)); 
+    await AsyncStorage.setItem('listOfDecks', 
+      JSON.stringify(listOfDecks)); 
+}
+
+export async function addDeck (deckTitle) { 
+  let deck = {}
+  deck[deckTitle]= {
+    questions: []
+  }
+  let listOfDecks = await getDecks();
+
+  listOfDecks[deckTitle] = { 
+    title: deckTitle,
+    questions:[] 
   }
 
-// async _addDeck () { 
-//   const listOfDecks = [...this.state.listOfDecks, this.state.text]; 
+  await AsyncStorage.setItem('listOfDecks', 
+    JSON.stringify(listOfDecks)); 
 
-//   await AsyncStorage.setItem('listOfDecks', 
-//     JSON.stringify(listOfDecks)); 
+}
 
-//   this._getDecks(); 
-// } 
+export async function addCardToDeck (card, deck) {
+  deck.questions.push(card);
+  let listOfDecks = await getDecks();
+  listOfDecks[deck["title"]] = deck;
 
-// async _setInitialState () { 
-//   const listOfDecks = this.state.listOfDecks;
+  await AsyncStorage.setItem('listOfDecks', 
+    JSON.stringify(listOfDecks)); 
+}
 
-//   await AsyncStorage.setItem('listOfDecks', 
-//     JSON.stringify(listOfDecks)); 
-
-//   this._getDecks();
-// } 
-
-// getDecks() {
-//   AsyncStorage.getAllKeys();
-// }
-
-// TODO: implement getDecks
-// TODO: implement getDeck
-// TODO: implement saveDeckTitle
-// TODO: implement addCardToDeck
+export async function getDeck (deckId) { 
+  const listOfDecks = await getDecks();
+  return listOfDecks[deckId];
+}
 
